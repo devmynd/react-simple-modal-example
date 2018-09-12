e422713819e16c48fb4a4ec8f604adf588b16422
Michael Crismali <michael.crismali@gmail.com>
2018-09-12 11:18:25 -0500

    Added some descriptive elements, button styles

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to a Simple Modal Example</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To open the modal, click the button below.
         </p>
+        <button className="button">Open Modal</button>
       </div>
     );
   }
